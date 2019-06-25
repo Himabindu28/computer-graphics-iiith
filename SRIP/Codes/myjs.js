2b2c4ef8a43858237543b038711abfbd8606f603
Himabindu28 <43909842+Himabindu28@users.noreply.github.com>
2019-06-25 19:54:30 +0530

    Add files via upload

--- SRIP/Codes/myjs.js
+++ SRIP/Codes/myjs.js
@@ -1,6 +1,6 @@
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera( 60, (0.7*window.innerWidth) / (0.95*window.innerHeight), 0.001, 100000 );
+var camera = new THREE.PerspectiveCamera( 60, (0.7*window.innerWidth) / (0.7*window.innerHeight), 0.001, 100000 );
 
 var camera2 = new THREE.OrthographicCamera(	window.innerWidth / -900, 
 											window.innerWidth / 900, 
@@ -11,14 +11,15 @@ var camera2 = new THREE.OrthographicCamera(	window.innerWidth / -900,
 
 
 var renderer = new THREE.WebGLRenderer();
-var heightScreen = window.innerHeight;
-var widthScreen = window.innerWidth;
+var heightScreen = 0.7*window.innerHeight;
+var widthScreen = 0.5*window.innerWidth;
 renderer.setSize( widthScreen, heightScreen);
 document.body.appendChild( renderer.domElement );
 
+
 window.addEventListener( 'resize' , function () {
-	var width = window.innerWidth;
-	var height =window.innerHeight;
+	var width = 0.5*window.innerWidth;
+	var height = 0.7*window.innerHeight;
 	renderer.setSize( width, height);
 	camera.aspect = width / height;
 	camera.updateProjectionMatrix();
@@ -130,6 +131,10 @@ var s37 = new THREE.Mesh( sg, sm );
 var s38 = new THREE.Mesh( sg, sm );
 var s39 = new THREE.Mesh( sg, sm );
 var s40 = new THREE.Mesh( sg, sm );
+var s41 = new THREE.Mesh( sg, sm );
+var s42 = new THREE.Mesh( sg, sm );
+var s43 = new THREE.Mesh( sg, sm );
+
 
 function lockV() {
 	var condition = document.getElementById('lockVertices').checked;
@@ -324,6 +329,8 @@ function ActiveDodecahedron() {
 		scene.add(s27);
 		scene.add(s12);
 		scene.add(s28);
+		
+		DodecTActive();
 	} else {
 		scene.remove(s9);
 		scene.remove(s10);
@@ -349,7 +356,60 @@ function ActiveDodecahedron() {
 }
 
 
+function DodecTActive()
+{		
+	var dx = Number(document.getElementById('DodecX').value);
+	var dy = Number(document.getElementById('DodecY').value);
+	var dz = Number(document.getElementById('DodecZ').value);
+		s9.position.set(0.1  + 0.1*dx, 0.1 +0.1*dy,0.1*dz+ 0.1);
+		s10.position.set(0.1 + 0.1*dx, 0.1 +0.1*dy,0.1*dz -0.1);
+		s11.position.set(0.1 + 0.1*dx, -0.1+0.1*dy,0.1*dz+ 0.1);
+		s12.position.set(0.1 + 0.1*dx, -0.1+0.1*dy,0.1*dz -0.1);
+		s13.position.set(-0.1+ 0.1*dx, -0.1+0.1*dy,0.1*dz -0.1);
+		s14.position.set(-0.1+ 0.1*dx, 0.1 +0.1*dy,0.1*dz -0.1);
+		s15.position.set(-0.1+ 0.1*dx, -0.1+0.1*dy,0.1*dz+ 0.1);
+		s16.position.set(-0.1+ 0.1*dx, 0.1 +0.1*dy,0.1*dz+ 0.1);
 
+		s17.position.set(0+0.1*dx, 0.1618 +0.1*dy, 0.1*dz+0.0618);
+		s18.position.set(0+0.1*dx, -0.1618+0.1*dy, 0.1*dz-0.0618);
+		s19.position.set(0+0.1*dx, 0.1618 +0.1*dy, 0.1*dz-0.0618);
+		s20.position.set(0+0.1*dx, -0.1618+0.1*dy, 0.1*dz+0.0618);
+
+		s21.position.set(0.0618 +0.1*dx, 0+0.1*dy,0.1*dz+0.1618);
+		s22.position.set(-0.0618+0.1*dx, 0+0.1*dy,0.1*dz-0.1618);
+		s23.position.set(-0.0618+0.1*dx, 0+0.1*dy,0.1*dz+0.1618);
+		s24.position.set(0.0618 +0.1*dx, 0+0.1*dy,0.1*dz-0.1618);
+
+		s25.position.set(0.1*dx+0.1618,0.1*dy+0.0618,0.1*dz+0);
+		s26.position.set(0.1*dx-0.1618,0.1*dy-0.0618,0.1*dz+0);
+		s27.position.set(0.1*dx+0.1618,0.1*dy-0.0618,0.1*dz+0);
+		s28.position.set(0.1*dx-0.1618,0.1*dy+0.0618,0.1*dz+0);
+		
+		scene.add(s9);
+		scene.add(s10);
+		scene.add(s11);
+		scene.add(s12);
+		scene.add(s13);
+		scene.add(s14);
+		scene.add(s15);
+		scene.add(s16);
+		scene.add(s17);
+		scene.add(s18);
+		scene.add(s19);
+		scene.add(s20);
+		scene.add(s21);
+		scene.add(s22);
+		scene.add(s23);
+		scene.add(s24);
+		scene.add(s25);
+		scene.add(s26);
+		scene.add(s27);
+		scene.add(s12);
+		scene.add(s28);
+
+		document.getElementById('DodecahedronActive').checked = true;
+
+}
 
 
 
@@ -428,6 +488,7 @@ function ActiveIcosahedron() {
 		scene.add(s38);
 		scene.add(s39);
 		scene.add(s40);
+		IcosaTActive();
 
 	} else {
 		scene.remove(s29);
@@ -445,7 +506,40 @@ function ActiveIcosahedron() {
 	}
 }
 
+function IcosaTActive()
+{
+	var ix = Number(document.getElementById('IcoX').value);
+	var iy = Number(document.getElementById('IcoY').value);
+	var iz = Number(document.getElementById('IcoZ').value);
 
+
+		 s29.position.set(0.1*ix+0.05236, 0.1*iy+ 0.0,     0.1*iz+0.08472);
+		 s30.position.set(0.1*ix-0.05236, 0.1*iy+ 0.0,     0.1*iz+0.084726);
+		 s31.position.set(0.1*ix+0.0,     0.1*iy+ 0.084726,0.1*iz+0.05236);
+		 s32.position.set(0.1*ix+0.0,     0.1*iy -0.084726,0.1*iz+0.05236);
+		 s33.position.set(0.1*ix+0.084726,0.1*iy+ 0.05236, 0.1*iz+0.0);
+		 s34.position.set(0.1*ix-0.084726,0.1*iy+ 0.05236, 0.1*iz+0.0);
+		 s35.position.set(0.1*ix-0.084726,0.1*iy -0.05236, 0.1*iz+0.0);
+		 s36.position.set(0.1*ix+0.084726,0.1*iy -0.05236, 0.1*iz+0.0);
+		 s37.position.set(0.1*ix+0.0,     0.1*iy+ 0.084726,0.1*iz-0.05236) ;
+		s38.position.set(0.1*ix+0.0, -0.084726+0.1*iy,0.1*iz-0.05236);
+		 s39.position.set(0.1*ix+0.05236, 0.0 +0.1*iy,0.1*iz-0.084726);
+		 s40.position.set(0.1*ix-0.05236, 0.0 +0.1*iy,0.1*iz-0.084726); 
+
+		scene.add(s29);
+		scene.add(s30);
+		scene.add(s31);
+		scene.add(s32);
+		scene.add(s33);
+		scene.add(s34);
+		scene.add(s35);
+		scene.add(s36);
+		scene.add(s37);
+		scene.add(s38);
+		scene.add(s39);
+		scene.add(s40);
+		document.getElementById('IcosahedronActive').checked = true;
+}
 
 
 
@@ -468,6 +562,61 @@ function DrawCube() {
 		document.getElementById('CubeActive').checked = false;
 		ActiveCube();
 	}
+}
+var geom = new THREE.Geometry();
+var v1 = new THREE.Vector3(0.4,0.3,0);
+var v2 = new THREE.Vector3(0,0.4,0);
+var v3 = new THREE.Vector3(0.2,0.1,0);
+geom.vertices.push(v1);
+geom.vertices.push(v2);
+geom.vertices.push(v3);
+
+
+geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
+
+var geometry5=new THREE.Triangle(v1,v2,v3);
+var material = new THREE.MeshBasicMaterial( { color: 0x099556 } );
+var triangle = new THREE.Mesh( geom, material );
+
+
+function DrawTriangle(){
+	var condition = document.getElementById('TriangleDraw').checked;
+	if( condition == true) {
+
+		scene.add(triangle );
+
+
+	} else {
+		scene.remove(triangle);
+		document.getElementById('TriangleActive').checked = false;
+		ActiveTriangle();
+	}
+
+}
+function ActiveTriangle(){
+
+	var condition1 = document.getElementById('TriangleActive').checked;
+	var condition2 = document.getElementById('TriangleDraw').checked;
+	if( condition1 == true && condition2 == true) {
+		//cube.position.set(0,0,0);
+
+		s41.position.set(0.4,0.3,0);
+		s42.position.set(0,0.4,0);
+		s43.position.set(0.2,0.1,0);
+		
+		
+		scene.add(s41);
+		scene.add(s42);
+		scene.add(s43);
+
+		TriTActive();
+	} else {
+		scene.remove(s41);
+		scene.remove(s42);
+		scene.remove(s43);
+
+	}
+
 }
 
 // Vertices of the figures in the world
@@ -494,6 +643,7 @@ function ActiveCube() {
 		scene.add(s6);
 		scene.add(s7);
 		scene.add(s8);
+		CubeTActive();
 	} else {
 		scene.remove(s1);
 		scene.remove(s2);
@@ -526,17 +676,29 @@ function CubeTrans() {
 	//var cube = new THREE.Mesh( geometry4, material );
 	
 	//scene.add(cube);
-	cube.position.x = cx*0.1;
+		cube.position.x = cx*0.1;
 	cube.position.y = cy*0.1;
 	cube.position.z = cz*0.1;
-	s1.position.set(0.1+cx*0.1, 0.1+cy*0.1, -0.1+cz*0.1);
-	s2.position.set(0.1+cx*0.1, 0.1+cy*0.1, 0.1+cz*0.1);
-	s3.position.set(-0.1+cx*0.1, 0.1+cy*0.1, -0.1+cz*0.1);
-	s4.position.set(0.1+cx*0.1, -0.1+cy*0.1, 0.1+cz*0.1);
-	s5.position.set(0.1+cx*0.1, -0.1+cy*0.1, -0.1+cz*0.1);
-	s6.position.set(-0.1+cx*0.1, -0.1+cy*0.1, 0.1+cz*0.1);
-	s7.position.set(-0.1+cx*0.1, 0.1+cy*0.1, 0.1+cz*0.1);
-	s8.position.set(-0.1+cx*0.1, -0.1+cy*0.1, -0.1+cz*0.1);
+
+}
+	// body...
+}
+
+function CubeTActive(){
+	
+
+	var cx = Number(document.getElementById('CubeX').value);
+	var cy = Number(document.getElementById('CubeY').value);
+	var cz = Number(document.getElementById('CubeZ').value);
+	var sx=(document.getElementById('sliderStatus').innerHTML);
+	s1.position.set(sx*0.1+cx*0.1 *sx, 0.1+cy*0.1, -0.1+cz*0.1);
+	s2.position.set(sx*0.1+cx*0.1 *sx, 0.1+cy*0.1, 0.1+cz*0.1);
+	s3.position.set(sx*-0.1+cx*0.1*sx, 0.1+cy*0.1, -0.1+cz*0.1);
+	s4.position.set(sx*0.1+cx*0.1 *sx, -0.1+cy*0.1, 0.1+cz*0.1);
+	s5.position.set(sx*0.1+cx*0.1 *sx, -0.1+cy*0.1, -0.1+cz*0.1);
+	s6.position.set(sx*-0.1+cx*0.1*sx, -0.1+cy*0.1, 0.1+cz*0.1);
+	s7.position.set(sx*-0.1+cx*0.1*sx, 0.1+cy*0.1, 0.1+cz*0.1);
+	s8.position.set(sx*-0.1+cx*0.1*sx, -0.1+cy*0.1, -0.1+cz*0.1);
 		scene.add(s1);
 		scene.add(s2);
 		scene.add(s3);
@@ -546,10 +708,100 @@ function CubeTrans() {
 		scene.add(s7);
 		scene.add(s8);
 	document.getElementById('CubeActive').checked = true;
+}
 
+
+function TriTActive()
+{
+	var tx = Number(document.getElementById('TriX').value);
+	var ty = Number(document.getElementById('TriY').value);
+	var tz = Number(document.getElementById('TriZ').value);
+	var sx=(document.getElementById('sliderStatus').innerHTML);
+	s41.position.set(0.4+tx*0.1 , 0.3+ty*0.1, 0+tz*0.1);
+	s42.position.set(0+tx*0.1 , 0.4+ty*0.1, 0+tz*0.1);
+	s43.position.set(0.2+tx*0.1, 0.1+ty*0.1, 0+tz*0.1);
+	
+	scene.add(s41);
+	scene.add(s42);
+	scene.add(s43);
+
+	document.getElementById('TriangleActive').checked = true;
 }
-	// body...
+
+
+
+
+function sliderChange(val){
+    document.getElementById('sliderStatus').innerHTML=val; 
 }
+
+
+
+function addScale() {
+	var condition= document.getElementById('CubeDraw');
+	var condition1= document.getElementById('DodecahedronDraw');
+	var condition2= document.getElementById('IcosahedronDraw');
+	var condition3=document.getElementById('TriangleDraw');
+    var sx=(document.getElementById('sliderStatus').innerHTML);
+    var sy=(document.getElementById('sliderStatus').innerHTML);
+    if (isNaN(sx) || isNaN(sy)) {
+        //setErrorMessage("Scale inputs must be numbers!");
+        return;
+    }
+    if (sx == 0 || sy == 0) {
+        //setErrorMessage("Enter a non-zero scale factors!");
+        return;
+    }
+    if(condition)
+			{	if(sx<=2&&sy<=3){
+				cube.scale.set(sx,sy,1);}
+				else if(sy<=3 && sx>2){
+					cube.scale.set(2,sy,1);
+				}else{
+					cube.scale.set(2,3,1);
+				}
+				document.getElementById('CubeActive').checked = false;
+				ActiveCube();
+			}
+		if(condition1)
+			{	if(sx<=2&&sy<=3){
+				icosahedron.scale.set(sx,sy,1);}
+				else if(sy<=3 && sx>2){
+					icosahedron.scale.set(2,sy,1);
+				}
+				else{
+				icosahedron.scale.set(2,3,1);
+			}
+				document.getElementById('IcosahedronActive').checked = false;
+				ActiveIcosahedron();
+			}
+		if(condition2)
+			{	if(sx<=2&&sy<=3){
+				dodecahedron.scale.set(sx,sy,1);}
+				else if(sy<=3 && sx>2){
+					dodecahedron.scale.set(2,sy,1);
+				}
+				else{
+				dodecahedron.scale.set(2,3,1);
+			}
+				document.getElementById('DodecahedronActive').checked = false;
+				ActiveDodecahedron();
+			}
+		if(condition3)
+		{	if(sx<=2&&sy<=3){
+				triangle.scale.set(sx,sy,1);}
+				else if(sy<=3 && sx>2){
+					triangle.scale.set(2,sy,1);
+				}
+				else{
+			triangle.scale.set(2,3,1);}
+			document.getElementById('TriangleActive').checked = false;
+			ActiveTriangle();
+		}
+}
+
+
+
 
 function DodecTrans(){
 	var condition1 = document.getElementById('DodecahedronDraw').checked;
@@ -566,54 +818,7 @@ function DodecTrans(){
 	dodecahedron.position.y = dy*0.1;
 	dodecahedron.position.z = dz*0.1;
 
-		s9.position.set(0.1  + 0.1*dx, 0.1 +0.1*dy,0.1*dz+ 0.1);
-		s10.position.set(0.1 + 0.1*dx, 0.1 +0.1*dy,0.1*dz -0.1);
-		s11.position.set(0.1 + 0.1*dx, -0.1+0.1*dy,0.1*dz+ 0.1);
-		s12.position.set(0.1 + 0.1*dx, -0.1+0.1*dy,0.1*dz -0.1);
-		s13.position.set(-0.1+ 0.1*dx, -0.1+0.1*dy,0.1*dz -0.1);
-		s14.position.set(-0.1+ 0.1*dx, 0.1 +0.1*dy,0.1*dz -0.1);
-		s15.position.set(-0.1+ 0.1*dx, -0.1+0.1*dy,0.1*dz+ 0.1);
-		s16.position.set(-0.1+ 0.1*dx, 0.1 +0.1*dy,0.1*dz+ 0.1);
-
-		s17.position.set(0+0.1*dx, 0.1618 +0.1*dy, 0.1*dz+0.0618);
-		s18.position.set(0+0.1*dx, -0.1618+0.1*dy, 0.1*dz-0.0618);
-		s19.position.set(0+0.1*dx, 0.1618 +0.1*dy, 0.1*dz-0.0618);
-		s20.position.set(0+0.1*dx, -0.1618+0.1*dy, 0.1*dz+0.0618);
-
-		s21.position.set(0.0618 +0.1*dx, 0+0.1*dy,0.1*dz+0.1618);
-		s22.position.set(-0.0618+0.1*dx, 0+0.1*dy,0.1*dz-0.1618);
-		s23.position.set(-0.0618+0.1*dx, 0+0.1*dy,0.1*dz+0.1618);
-		s24.position.set(0.0618 +0.1*dx, 0+0.1*dy,0.1*dz-0.1618);
-
-		s25.position.set(0.1*dx+0.1618,0.1*dy+0.0618,0.1*dz+0);
-		s26.position.set(0.1*dx-0.1618,0.1*dy-0.0618,0.1*dz+0);
-		s27.position.set(0.1*dx+0.1618,0.1*dy-0.0618,0.1*dz+0);
-		s28.position.set(0.1*dx-0.1618,0.1*dy+0.0618,0.1*dz+0);
 		
-		scene.add(s9);
-		scene.add(s10);
-		scene.add(s11);
-		scene.add(s12);
-		scene.add(s13);
-		scene.add(s14);
-		scene.add(s15);
-		scene.add(s16);
-		scene.add(s17);
-		scene.add(s18);
-		scene.add(s19);
-		scene.add(s20);
-		scene.add(s21);
-		scene.add(s22);
-		scene.add(s23);
-		scene.add(s24);
-		scene.add(s25);
-		scene.add(s26);
-		scene.add(s27);
-		scene.add(s12);
-		scene.add(s28);
-
-		document.getElementById('DodecahedronActive').checked = true;
-
 }
 
 }
@@ -635,35 +840,22 @@ function IcosaTrans() {
 	icosahedron.position.z = iz*0.1;
 
 
-		 s29.position.set(0.1*ix+0.05236, 0.1*iy+ 0.0,     0.1*iz+0.08472);
-		 s30.position.set(0.1*ix-0.05236, 0.1*iy+ 0.0,     0.1*iz+0.084726);
-		 s31.position.set(0.1*ix+0.0,     0.1*iy+ 0.084726,0.1*iz+0.05236);
-		 s32.position.set(0.1*ix+0.0,     0.1*iy -0.084726,0.1*iz+0.05236);
-		 s33.position.set(0.1*ix+0.084726,0.1*iy+ 0.05236, 0.1*iz+0.0);
-		 s34.position.set(0.1*ix-0.084726,0.1*iy+ 0.05236, 0.1*iz+0.0);
-		 s35.position.set(0.1*ix-0.084726,0.1*iy -0.05236, 0.1*iz+0.0);
-		 s36.position.set(0.1*ix+0.084726,0.1*iy -0.05236, 0.1*iz+0.0);
-		 s37.position.set(0.1*ix+0.0,     0.1*iy+ 0.084726,0.1*iz-0.05236) ;
-		s38.position.set(0.1*ix+0.0, -0.084726+0.1*iy,0.1*iz-0.05236);
-		 s39.position.set(0.1*ix+0.05236, 0.0 +0.1*iy,0.1*iz-0.084726);
-		 s40.position.set(0.1*ix-0.05236, 0.0 +0.1*iy,0.1*iz-0.084726); 
-
-		scene.add(s29);
-		scene.add(s30);
-		scene.add(s31);
-		scene.add(s32);
-		scene.add(s33);
-		scene.add(s34);
-		scene.add(s35);
-		scene.add(s36);
-		scene.add(s37);
-		scene.add(s38);
-		scene.add(s39);
-		scene.add(s40);
-		document.getElementById('IcosahedronActive').checked = true;
 }
 }
+function TriTrans(){
+	var condition1 = document.getElementById('TriangleDraw').checked;
+	if(condition1==true){
+	document.getElementById('TriangleActive').checked = false;
+	ActiveTriangle();
 
+	var tx = Number(document.getElementById('TriX').value);
+	var ty = Number(document.getElementById('TriY').value);
+	var tz = Number(document.getElementById('TriZ').value);
+	triangle.position.x = tx*0.1;
+	triangle.position.y = ty*0.1;
+	triangle.position.z = tz*0.1;
+
+}}
 
 //camera.position.set(0.5,-0.25,1.5)
 camera.position.set(0.5,-0.25,2.2);
@@ -1060,7 +1252,15 @@ function startTransistion() {
 	exmpl2.position.set(0.35+xd,-0.35+yd,0+zd);
 	exmple3.position.set(0.9+xd,-0.35+yd,0+zd);
 }
+	/*var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({
+        color: 0xffffff
+    }));
 
+    mesh.position.set(0, 0, 0);
+    mesh.scale.set(0.5, 0.5, 1);
+    mesh.castShadow = true;*/
+  
 function startAnimation() {
 
 	var conditionAnimation = document.getElementById('animation').checked;
@@ -1069,24 +1269,65 @@ function startAnimation() {
 		document.getElementById('lockVertices').checked = false;
 		lockV();
 
-		exmple1.position.set(0,-0.2,-0.05);
-		exmpl2.position.set(0,-0.1615,-0.35);
-		exmple3.position.set(0,0.1395,-0.625);
-		exmple3.rotation.x = 90*(Math.PI / 180);
-		exmpl2.rotation.y = 90*(Math.PI / 180);
-		exmpl2.rotation.x = 35*(Math.PI / 180);
+		var condition = document.getElementById('CubeDraw').checked;
+		var condition1 = document.getElementById('IcosahedronDraw').checked;
+		var condition2 = document.getElementById('DodecahedronDraw').checked;
+		var condition3 = document.getElementById('TriangleDraw').checked;
+		if(condition)
+			{
+				cube.scale.set(2,3,1);
+				document.getElementById('CubeActive').checked = false;
+				ActiveCube();
+
+			}
+		if(condition1)
+			{
+				icosahedron.scale.set(2,3,1);
+				document.getElementById('IcosahedronActive').checked = false;
+				ActiveIcosahedron();
+			}
+		if(condition2)
+		{
+			dodecahedron.scale.set(2,3,1);
+			document.getElementById('DodecahedronActive').checked = false;
+			ActiveDodecahedron();
+		}
+		if(condition3)
+		{
+			triangle.scale.set(2,3,1);
+			document.getElementById('TriangleActive').checked = false;
+			ActiveTriangle();
+		}
+
+
 	} else {
 		document.getElementById('lockVertices').checked = true;
 		lockV();
 
-		exmple3.rotation.x = 0*(Math.PI / 180);
-		exmpl2.rotation.y = 0*(Math.PI / 180);
-		exmpl2.rotation.x = 0*(Math.PI / 180);
-		exmple1.position.set(0.05,-0.2,0);
-		exmpl2.position.set(0.35,-0.35,0);
-		exmple3.position.set(0.9,-0.35,0);
+		var condition = document.getElementById('CubeDraw').checked;
+		var condition1 = document.getElementById('IcosahedronDraw').checked;
+		var condition2 = document.getElementById('DodecahedronDraw').checked;
+		var condition3 = document.getElementById('TriangleDraw').checked;
+		if(condition3)
+		{	
+			triangle.scale.set(1,1,1);
+
+		}
+		if(condition)
+			{
+				cube.scale.set(1,1,1);
+			}
+		if(condition1)
+			{
+				icosahedron.scale.set(1,1,1);
+			}
+		if(condition2)
+			{
+				dodecahedron.scale.set(1,1,1);
+			}
 	}
 }
+scene.add(triangle);
 
 													//GAME LOGIC
 
